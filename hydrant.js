21da963fa61f2b9d7d8265514b740865a94bc01c
danilkomyshev <danil.komyshev777@gmail.com>
2020-03-27 05:42:29 +0400

    Initial commit

---
+++ hydrant.js
@@ -0,0 +1,30 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [56.849283, 53.205247],
+            zoom: 12
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "https://raw.githubusercontent.com/danil99152/YMap/master/data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+}
