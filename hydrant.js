25b4a6e4923006bdae4dae4766b51f69182a827c
danilkomyshev <danil.komyshev777@gmail.com>
2020-03-30 03:54:48 +0400

    Добавлен метод поиска и объединения данных

--- hydrant.js
+++ hydrant.js
@@ -1,39 +1,54 @@
 ymaps.ready(init);
 
 function init () {
-    var myMap = new ymaps.Map('map', {
+    let myMap = new ymaps.Map('map', {
             center: [56.849283, 53.205247],
             zoom: 12
         }, {
             searchControlProvider: 'yandex#search'
         }),
         objectManager = new ymaps.ObjectManager({
-            // Чтобы метки начали кластеризоваться, выставляем опцию.
             clusterize: true,
-            // ObjectManager принимает те же опции, что и кластеризатор.
             gridSize: 32,
             clusterDisableClickZoom: true
         });
 
-    // Чтобы задать опции одиночным объектам и кластерам,
-    // обратимся к дочерним коллекциям ObjectManager.
     objectManager.objects.options.set('preset', 'islands#greenDotIcon');
     objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
     myMap.geoObjects.add(objectManager);
 
-    var url = "data.json";
-
-    $.getJSON(url,function(data){
-        for (i = 0; i < data.features.length; i++) {
-            defaultObject = data.features[i].properties.balloonContentBody;
-            data.features[i].properties.balloonContentBody = [];
-            data.features[i].properties.balloonContentBody = "<b>Тип гидранта:</b> "+ defaultObject.type + "<br>"
-            + "<b>Дата последней проверки:</b> " + defaultObject.last_date + "<br>"
-            + "<b>Мощность гидранта:</b> " + defaultObject.power + "<br>"
-            + "<b>Статус гидранта:</b > " + defaultObject.status + "<br>"
-            + "<img src=\"images/" + defaultObject.img + "\" width=\"150\" height=\"200\"><br>"
-            + "<b>Примечание:</b> " + defaultObject.notation;
-        }
-        objectManager.add(data);
-    });
+    let i = 0;
+    const macroUrl = "data.json";
+    function uniteData(){
+        i++;
+        let microUrl = "hydrants/" + i + ".json";
+        $.getJSON(microUrl, function (microData) {
+            let coordinates = microData.coordinates;
+            let address = microData.address;
+            let type = microData.type;
+            let last_date = microData.last_date;
+            let power = microData.power;
+            let status = microData.status;
+            let img = microData.img;
+            let notation = microData.notation;
+            $.getJSON(macroUrl, function (macroData) {
+                macroData.features[0].id = i;
+                macroData.features[0].geometry.coordinates = coordinates;
+                macroData.features[0].properties.balloonContentHeader = address;
+                macroData.features[0].properties.balloonContentBody = [];
+                macroData.features[0].properties.balloonContentBody = "<b>Тип гидранта:</b> " + type + "<br>"
+                    + "<b>Дата последней проверки:</b> " + last_date + "<br>"
+                    + "<b>Мощность гидранта:</b> " + power + "<br>"
+                    + "<b>Статус гидранта:</b > " + status + "<br>"
+                    + "<img src=\"images/" + img + "\" width=\"150\" height=\"200\"><br>"
+                    + "<b>Примечание:</b> " + notation;
+                objectManager.add(macroData);
+            });
+        }).done(function () {
+            uniteData();
+        }).fail(function() {
+            console.log("Если это сообщение вышло дважды, то, возможно, проблема в синтаксисе нового json-файла");
+        });
+    }
+    uniteData();
 }
